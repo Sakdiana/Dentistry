77a93260e84a6f4415796cf4ce6bdcf12196a480
Sakdiana <bekmurzaevasakdiana2@gmail.com>
2025-07-17 15:10:16 +0600

    installing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+
+      <p className='text-red-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste mollitia distinctio aperiam!</p>
+    </>
+  )
+}
+
+export default App
